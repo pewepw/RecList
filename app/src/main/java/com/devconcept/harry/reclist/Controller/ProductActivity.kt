4410309e13840a2da85219d12c95fc79a0a8626d
Harry <poripori1030@gmail.com>
2017-11-23 02:13:48 +0800

    Finished

--- app/src/main/java/com/devconcept/harry/reclist/Controller/ProductActivity.kt
+++ app/src/main/java/com/devconcept/harry/reclist/Controller/ProductActivity.kt
@@ -1,17 +1,40 @@
 package com.devconcept.harry.reclist.Controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import com.devconcept.harry.reclist.Adapter.ProductAdapter
 import com.devconcept.harry.reclist.R
+import com.devconcept.harry.reclist.Services.DataService
 import com.devconcept.harry.reclist.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_product.*
 
 class ProductActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductAdapter(this, DataService.getProducts(categoryType))
+
+        var spanCount = 2
+
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720) {
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productListView.layoutManager = layoutManager
+        productListView.adapter = adapter
     }
 }
--- app/src/main/java/com/devconcept/harry/reclist/Services/DataService.kt
+++ app/src/main/java/com/devconcept/harry/reclist/Services/DataService.kt
@@ -11,17 +11,43 @@ object DataService {
             Category("SHIRTS", "shirtimage"),
             Category("HOODIES", "hoodieimage"),
             Category("HATS", "hatimage"),
+            Category("DIGITAL", "digitalgoodsimage"),
+            Category("SHIRTS", "shirtimage"),
+            Category("HOODIES", "hoodieimage"),
+            Category("HATS", "hatimage"),
+            Category("DIGITAL", "digitalgoodsimage"),
+            Category("SHIRTS", "shirtimage"),
+            Category("HOODIES", "hoodieimage"),
+            Category("HATS", "hatimage"),
             Category("DIGITAL", "digitalgoodsimage")
+
     )
 
     val hats = listOf(
             Product("Adidas Hat", "RM90", "hat1"),
             Product("Nike Hat", "RM110", "hat2"),
             Product("Puma Hat", "RM82", "hat3"),
+            Product("Under Amour Hat", "RM150", "hat4"),
+            Product("Adidas Hat", "RM90", "hat1"),
+            Product("Nike Hat", "RM110", "hat2"),
+            Product("Puma Hat", "RM82", "hat3"),
+            Product("Under Amour Hat", "RM150", "hat4"),
+            Product("Adidas Hat", "RM90", "hat1"),
+            Product("Nike Hat", "RM110", "hat2"),
+            Product("Puma Hat", "RM82", "hat3"),
             Product("Under Amour Hat", "RM150", "hat4")
+
     )
 
     val hoodies = listOf(
+            Product("Adidas Hoodie", "RM90", "hoodie1"),
+            Product("Nike Hoodie", "RM110", "hoodie2"),
+            Product("Puma Hoodie", "RM82", "hoodie3"),
+            Product("Under Amour Hoodie", "RM150", "hoodie4"),
+            Product("Adidas Hoodie", "RM90", "hoodie1"),
+            Product("Nike Hoodie", "RM110", "hoodie2"),
+            Product("Puma Hoodie", "RM82", "hoodie3"),
+            Product("Under Amour Hoodie", "RM150", "hoodie4"),
             Product("Adidas Hoodie", "RM90", "hoodie1"),
             Product("Nike Hoodie", "RM110", "hoodie2"),
             Product("Puma Hoodie", "RM82", "hoodie3"),
@@ -33,17 +59,27 @@ object DataService {
             Product("Nike Shirt", "RM110", "shirt2"),
             Product("Puma Shirt", "RM82", "shirt3"),
             Product("Under Amour Shirt", "RM150", "shirt4"),
+            Product("Gucci Shirt", "RM200", "shirt5"),
+            Product("Adidas Shirt", "RM90", "shirt1"),
+            Product("Nike Shirt", "RM110", "shirt2"),
+            Product("Puma Shirt", "RM82", "shirt3"),
+            Product("Under Amour Shirt", "RM150", "shirt4"),
+            Product("Gucci Shirt", "RM200", "shirt5"),
+            Product("Adidas Shirt", "RM90", "shirt1"),
+            Product("Nike Shirt", "RM110", "shirt2"),
+            Product("Puma Shirt", "RM82", "shirt3"),
+            Product("Under Amour Shirt", "RM150", "shirt4"),
             Product("Gucci Shirt", "RM200", "shirt5")
     )
 
     val digitalGood = listOf<Product>()
 
     fun getProducts(category: String): List<Product> {
-        when(category) {
-            "SHIRTS" -> return shirts
-            "HATS" -> return hats
-            "HOODIES" -> return hoodies
-            else -> return digitalGood
+        return when(category) {
+            "SHIRTS" -> shirts
+            "HATS" -> hats
+            "HOODIES" -> hoodies
+            else -> digitalGood
         }
     }
 }
